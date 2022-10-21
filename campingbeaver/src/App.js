82c3323f8c6c5a46ce93f12c655dd5be88987ea7
주상민 <112377313+js4183@users.noreply.github.com>
2022-10-21 17:47:41 +0900

    폴더나누기/주상민

--- campingbeaver/src/App.js
+++ campingbeaver/src/App.js
@@ -1,12 +1,20 @@
-import Layout from './components/Layout/Layout'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-
+import Layout from './components/Layout/Layout'
+import Login from './pages/Login/Login'
+import Mypage from "./pages/Mypage/Mypage";
+import Reservation from "./pages/Reservation/Reservation";
 
 function App() {
   return (
-  <div>
-    <Layout/>
-  </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>} />
+        <Route path="/Login" element={<Login/>}/>
+        <Route path="/mypage" element={<Mypage/>}/>
+        <Route path="/Reservation" element={<Reservation/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
