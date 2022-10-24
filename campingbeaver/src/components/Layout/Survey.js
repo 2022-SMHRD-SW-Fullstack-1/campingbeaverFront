795f78b4759bef4c06049673212aa0ac4bb6a3b6
yeonjuko <yjoogo@gmail.com>
2022-10-24 09:58:21 +0900

    설문조사업데이트/연주

--- campingbeaver/src/components/Layout/Survey.js
+++ campingbeaver/src/components/Layout/Survey.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import Alert from 'react-bootstrap/Alert';
+import styles from './Survey.module.scss';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import {Route, Routes, Link} from 'react-router-dom'
 
 const Survey = () => {
+
   return (
-    <Alert variant="success">
-    <Alert.Heading>어서와 캠핑은 처음이지?</Alert.Heading>
+    <div style={{position: 'absolute', zIndex:3 ,margin: '3em', width: '50vw', height: '40vh', top:'30%', left:'20%'}}>
+    <Alert variant="success" className={styles.overlay}>
+    <Alert.Heading>캠핑 장비가 있으신가요?</Alert.Heading>
     <p>
-      여기서 함께할 친구를 골라보렴!
+     장비를 직접 선택하시려면 '예'를 선택하세요
     </p>
     <hr />
     <p className="mb-0">
-      Whenever you need to, be sure to use margin utilities to keep things
-      nice and tidy.
+    <div className="d-grid gap-2">
+      <Button variant="primary" size="lg"><Link to="/surveysecond" style={{ textDecoration: "none", color: 'white' }}>
+        예</Link>
+      </Button>
+      <Button variant="secondary" size="lg"><Link to="/surveythird" style={{ textDecoration: "none", color: 'white' }}>
+        아니오</Link>
+      </Button>
+    </div>
     </p>
   </Alert>
+ </div>
   )
 }
 
