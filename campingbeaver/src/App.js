6b601cdc9f9c10aa3837c008b98152463e7d666f
임아해 <limahae1015@gmail.com>
2022-10-27 15:37:40 +0900

    머지 전 커밋 /아해

--- campingbeaver/src/App.js
+++ campingbeaver/src/App.js
@@ -2,11 +2,10 @@
 import './App.css';
 import {BrowserRouter,Routes, Route} from 'react-router-dom';
 import Login from './auth/Login';
-import Signup from './auth/Signup';
+import SignUp from './auth/SignUp';
 import ProductList from './auth/ProductList';
 import Store from './auth/Store';
 import Layout from './components/Layout/Layout'
-
 import MyPage from "./pages/MyPage/MyPage";
 import Reservation from "./pages/Reservation/Reservation"; 
 import ReviewForm from "../src/pages/ReviewUpload/ReviewForm"
@@ -34,10 +33,10 @@ function App() {
         <Route path="/MyPage" element={<MyPage/>}/>
         <Route path="/Reservation" element={<Reservation/>}/>
         <Route path="/ReviewForm" element={<ReviewForm/>}/>
-        <Route path="Signup" element={<Signup/>} />
-        <Route path="Login" element={<Login/>} />
-        <Route path="ProductList" element={<ProductList/>} />
-        <Route path="Store" element={<Store/>} />
+        <Route path="/Signup" element={<SignUp/>} />
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/ProductList" element={<ProductList/>} />
+        <Route path="/Store" element={<Store/>} />
       </Routes>
     </BrowserRouter>
   )
