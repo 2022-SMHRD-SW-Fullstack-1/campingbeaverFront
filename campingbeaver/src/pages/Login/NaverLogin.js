5e404a7d63833d4798f491016d65b2b45909fb0a
주상민 <112377313+js4183@users.noreply.github.com>
2022-10-28 17:52:03 +0900

    로고 수정/상민

--- campingbeaver/src/pages/Login/NaverLogin.js
+++ campingbeaver/src/pages/Login/NaverLogin.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react'
-import naverImg from '../../components/img/naverlogin.png'
+import './Login.css';
+import naverImg from '../../components/img/naverLogo.png'
+
 const NaverLogin = () => {
 
-    
+
     //const [userInfo, setUserInfo] = useState();
     const { naver } = window
     const NAVER_CLIENT_ID = 'ouUd18EIec7BAaiUuI6P'
@@ -15,8 +17,8 @@ const NaverLogin = () => {
             // 팝업창으로 로그인 진행할 것인지?
             isPopup: false,
             // 버튼타입 
-            loginButton: { color: 'green', type: 3, height: 40 },
-			callbackHandle: true,
+            loginButton: { color: 'green', type: 3, height: 45 },
+            callbackHandle: true,
         })
         naverLogin.init()
 
@@ -26,11 +28,11 @@ const NaverLogin = () => {
 
         // 아래와 같이 로그인한 유저 ( 사용자 ) 정보를 직접 접근하여 추출가능하다.
         // 이때, 데이터는 첫 연동시 정보 동의한 데이터만 추출 가능하다.
-    
+
         // 백엔드 개발자가 정보를 전달해준다면 아래 요기! 라고 작성된 부분까지는 
         // 코드 생략이 가능하다.  
 
-        
+
         naverLogin.getLoginStatus(async function (status) {
             if (status) {
                 // 아래처럼 선택하여 추출이 가능하고,
@@ -43,43 +45,42 @@ const NaverLogin = () => {
                 console.log(naverLogin.user)
             }
         })
-        
+
         // 여기까지!
     }
 
-        // 네이버 소셜 로그인은 URL에 엑세스 토큰이 붙어서 전달됨
-        // 아래와 같이 토큰 추출 가능
-        const userAccessToken = () => {
-            window.location.href.includes('access_token') && getToken()
-        }
+    // 네이버 소셜 로그인은 URL에 엑세스 토큰이 붙어서 전달됨
+    // 아래와 같이 토큰 추출 가능
+    const userAccessToken = () => {
+        window.location.href.includes('access_token') && getToken()
+    }
 
-        const getToken = () => {
-            const token = window.location.href.split('=')[1].split('&')[0]
-            console.log(token);
+    const getToken = () => {
+        const token = window.location.href.split('=')[1].split('&')[0]
+        console.log(token);
 
-            // 이후 로컬 스토리지 또는 state에 저장하여 사용하자!   
-                // localStorage.setItem('access_token', token)
-		        // setGetToken(token)
-        }
+        // 이후 로컬 스토리지 또는 state에 저장하여 사용하자!   
+        // localStorage.setItem('access_token', token)
+        // setGetToken(token)
+    }
 
-        // 화면 첫 렌더링 이후 바로 실행하기 위해 useEffect 사용
-        useEffect(() => {
-            initializeNaverLogin()
-            userAccessToken()
-        },[])
+    const handleNaverClick = () => {
+        const naverLoginButton = document.getElementById("naverIdLogin_loginButton");
+        if (naverLoginButton) naverLoginButton.click();
+    }
 
-       
-  return (
-    <>
-        {/* 태그에 id="naverIdLogin"를 해주지 않으면 오류발생 */}
-			<div id="naverIdLogin"></div>
-            
-            
-          
-            
-           
-    </>
-  )
+    // 화면 첫 렌더링 이후 바로 실행하기 위해 useEffect 사용
+    useEffect(() => {
+        initializeNaverLogin()
+        userAccessToken()
+    }, [])
+
+    return (
+        <>  
+            <div className='naverBtn' onClick={handleNaverClick}><img src={naverImg} width="40px"/><span className='naverSpan'>네이버 로그인 </span><span></span></div>
+            {/* 태그에 id="naverIdLogin"를 해주지 않으면 오류발생 */}
+            <div id="naverIdLogin" style={{display:"none"}}></div>
+        </>
+    )
 }
-
 export default NaverLogin
--- campingbeaver/src/pages/Recommendation/FilterButton.js
+++ campingbeaver/src/pages/Recommendation/FilterButton.js
@@ -16,7 +16,7 @@ const FilterButton = () => {
 
 
   const [inputText, setInputText] = useState([]);
-  let [btnActive, setBtnActive] = useState('passive');
+  let [btnActive, setBtnActive] = useState();
 
 
   const clickHandle = (e) => {
@@ -35,7 +35,7 @@ const FilterButton = () => {
     }
   }
 
-  const inputTextList = inputText.map(inputText => <Button variant="outline-dark">{inputText}</Button>)
+  const inputTextList = inputText.map(inputText => <Button variant="outline-dark" onClick={clickHandle}>{inputText}</Button>)
 
 
 const clearBtn = (e) => {
@@ -107,10 +107,6 @@ const clearBtn = (e) => {
 
        {inputTextList}
 
-        
-        
-        inputText.forEach(element)
-        <Button variant="outline-dark">{inputText}</Button>
       </Alert>
       ))}
 
