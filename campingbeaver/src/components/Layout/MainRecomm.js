0da6853b0fb9f63c9395a18439049ccab423e4cd
ahaelim <limahae1015@gmail.com>
2022-11-06 21:38:19 +0900

    메인 수정 /아해

---
+++ campingbeaver/src/components/Layout/MainRecomm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './MainRecomm.module.scss'
+
+const MainRecomm = () => {
+  return (
+    <div className={styles.recommContainer}>
+        <h1>New Package</h1>
+    </div>
+  )
+}
+
+export default MainRecomm
