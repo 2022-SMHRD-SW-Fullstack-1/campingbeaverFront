36adeb5cd110e1ed888a40631e165d2ce762afe8
jh <qlqlghk145@gmail.com>
2022-11-11 09:35:30 +0900

    메인풀전회원탈시로그아웃적용

--- campingbeaver/src/pages/Mypage/EditMyInfo.js
+++ campingbeaver/src/pages/Mypage/EditMyInfo.js
@@ -1,29 +1,27 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styles from './MyPage.module.scss'
-import './EditMyInfo.scss';
-import axios from 'axios';
+import React, { useState } from "react";
+import { useHref, useNavigate } from "react-router-dom";
+import styles from "./MyPage.module.scss";
+import "./EditMyInfo.scss";
+import axios from "axios";
+import Logout from "../member/Logout";
 
 const EditMyInfo = () => {
-  const userId = localStorage.getItem('userId')
-  const userName = localStorage.getItem('userName')
-  
-  
+  const userId = localStorage.getItem("userId");
+  const userName = localStorage.getItem("userName");
 
   const navigate = useNavigate();
   const [inputValue, setInputValue] = useState({
-    id: '',
-    pw1: '',
-    pw2: '',
-    name: '',
-    address: '',
-    address2: '',
-    phone: '',
-    email: '',
-    birthday: '',
+    id: "",
+    pw1: "",
+    pw2: "",
+    name: "",
+    address: "",
+    address2: "",
+    phone: "",
+    email: "",
+    birthday: "",
     checked: true,
   });
-
 
   const {
     id,
@@ -38,7 +36,7 @@ const EditMyInfo = () => {
     checked,
   } = inputValue;
 
-  const handleInput = e => {
+  const handleInput = (e) => {
     const { name, value } = e.target;
     setInputValue({
       ...inputValue,
@@ -49,63 +47,63 @@ const EditMyInfo = () => {
   const goToMain = () => {
     const time = new Date();
     let now = time.toTimeString();
-    fetch('/beaver/update', {
-      method: 'POST',
+    fetch("/beaver/update", {
+      method: "POST",
       body: JSON.stringify({
         user_id: userId,
         user_pw: pw2,
         user_name: userName,
-        user_phone: '010' + phone,
+        user_phone: "010" + phone,
         user_addr: address + address2,
-        user_type: 'N',
+        user_type: "N",
         user_joindate: now,
         user_email: email,
       }),
     })
-      .then(response => {
+      .then((response) => {
         if (response.ok) {
           response.json();
         } else {
-          alert('입력창을 확인해 주세요');
+          alert("입력창을 확인해 주세요");
         }
       })
 
-      .then(result => {
-        navigate('/');
+      .then((result) => {
+        navigate("/");
       });
   };
 
   const handleDeleteProfile = () => {
     const time = new Date();
     let now = time.toTimeString();
-    console.log(userId)
+    console.log(userId);
     //e.preventDefault();
-    if (window.confirm('확인을 누르면 회원 정보가 삭제됩니다.')) {
-      fetch('/beaver/userdelete', {
-        method: 'POST',
+    if (window.confirm("확인을 누르면 회원 정보가 삭제됩니다.")) {
+      fetch("/beaver/userdelete", {
+        method: "POST",
         body: JSON.stringify({
           user_id: userId,
           user_pw: pw2,
           user_name: userName,
-          user_phone: '010' + phone,
+          user_phone: "010" + phone,
           user_addr: address + address2,
-          user_type: 'N',
+          user_type: "N",
           user_joindate: now,
           user_email: email,
         }),
       })
-      // axios
-      //   .post(`/beaver/userdelete`,
-      //     {
-      //       headers: {
-      //         Authorization: 'Bearer ' + userId,
-      //       },
-      //     }
-      //   )
+        // axios
+        //   .post(`/beaver/userdelete`,
+        //     {
+        //       headers: {
+        //         Authorization: 'Bearer ' + userId,
+        //       },
+        //     }
+        //   )
         .then(() => {
           localStorage.clear();
-          alert('그동안 이용해주셔서 감사합니다.');
-          navigate('/');
+          alert("그동안 이용해주셔서 감사합니다.");
+          navigate("/Logout");
         })
         .catch((err) => alert(err.response.data.message));
     } else {
@@ -115,32 +113,24 @@ const EditMyInfo = () => {
 
   return (
     <div>
-        <div>
-          <h1>
-          Edit Member Info
-          </h1>
-          <p className={styles.contact1}>회원 정보 수정</p>
-        </div>
+      <div>
+        <h1>Edit Member Info</h1>
+        <p className={styles.contact1}>회원 정보 수정</p>
+      </div>
 
-        
-        <div>
-
-
+      <div>
         {/* <main className="update"> */}
         <h1 className="title"></h1>
-
 
         <title className="information">
           <h2>기본정보</h2>
           <div>*필수입력사항</div>
         </title>
 
-       
-
         <form className="inputLine">
           <div className="inputTitle">ID</div>
           <div name="id">{userId}</div>
-          
+
           <div className="inputDescription"></div>
         </form>
 
@@ -161,7 +151,6 @@ const EditMyInfo = () => {
             type="password"
             className="userInput"
             onChange={handleInput}
-            
             name="pw2"
           />
           <div className="inputDescription"></div>
@@ -170,7 +159,6 @@ const EditMyInfo = () => {
         <form className="inputLine">
           <div className="inputTitle">Name</div>
           <div>{userName}</div>
-          
         </form>
 
         <form className="addressLine">
@@ -195,8 +183,6 @@ const EditMyInfo = () => {
           <div className="addressDescription">나머지 주소 (선택사항)</div>
         </form>
 
-        
-
         <form className="phoneLine">
           <div className="phone">Mobile Phone</div>
           <select name="phone" className="phoneSelect">
@@ -211,7 +197,6 @@ const EditMyInfo = () => {
             type="text"
             className="phoneSecond"
             onChange={handleInput}
-            
             name="phone"
           />
           <div className="phoneDescription"></div>
@@ -224,29 +209,21 @@ const EditMyInfo = () => {
             className="userInput"
             name="email"
             onChange={handleInput}
-           
           />
           <div className="inputDescription"></div>
         </form>
 
-        
-        <button
-          onClick={goToMain}
-          className="editBtn"
-        >
+        <button onClick={goToMain} className="editBtn">
           정보 수정하기
         </button>
 
-        <button
-          onClick={handleDeleteProfile}
-          className="editBtn"
-        >
+        <button onClick={handleDeleteProfile} className="editBtn">
           회원 탈퇴하기
         </button>
-      {/* </main> */}
-        </div>
+        {/* </main> */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EditMyInfo
+export default EditMyInfo;
