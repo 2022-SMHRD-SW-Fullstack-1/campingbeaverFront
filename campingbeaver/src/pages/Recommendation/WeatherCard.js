223e8f3225e093e2d564e9767a9bbbc52ad60ec5
yeonjuko <yjoogo@gmail.com>
2022-11-11 09:14:16 +0900

    머지찾기/연주

--- campingbeaver/src/pages/Recommendation/WeatherCard.js
+++ campingbeaver/src/pages/Recommendation/WeatherCard.js
@@ -1,49 +1,54 @@
-import React, { useState } from 'react'
-import clouds from './weatherimg/clouds.png'
-import drizzle from './weatherimg/drizzle.png'
-import mist from './weatherimg/mist.png'
-import raining from './weatherimg/raining.png'
-import snow from './weatherimg/snow.png'
-import sunny from './weatherimg/sunny.png'
-import thunderstorm from './weatherimg/thunderstorm.png'
-import style from './Reservation.module.scss'
+import React, { useState } from "react";
+import clouds from "./weatherimg/clouds.png";
+import drizzle from "./weatherimg/drizzle.png";
+import mist from "./weatherimg/mist.png";
+import raining from "./weatherimg/raining.png";
+import snow from "./weatherimg/snow.png";
+import sunny from "./weatherimg/sunny.png";
+import thunderstorm from "./weatherimg/thunderstorm.png";
+import style from "./Reservation.module.scss";
 
-const WeatherCard = ({value}) => {
-    // const [iconId, setIconId] = useState(0)
-    // setIconId(value.weather[0].id === 800 ? 0 : (parseInt(value.weather[0].id) / 100).toFixed(0));
-    
-    const selectIcon = () => {
-        switch (value.weather[0].id === 800 ? 0 : parseInt((value.weather[0].id / 100).toFixed(0)))
-        {
-            case 0:
-              return <img src={sunny} height='50px'/>;
-            case 2:
-              return <img src={thunderstorm}  height='50px'/>;
-            case 3:
-              return <img src={drizzle}  height='50px'/>;
-            case 5:
-              return <img src={raining}  height='50px'/>;
-            case 6:
-              return <img src={snow} height='50px'/>;
-            case 7:
-              return <img src={mist} height='50px'/>;
-            case 8:
-              return <img src={clouds} height='50px'/>;
-          }
-        }
+const WeatherCard = ({ value }) => {
+  // const [iconId, setIconId] = useState(0)
+  // setIconId(value.weather[0].id === 800 ? 0 : (parseInt(value.weather[0].id) / 100).toFixed(0));
+
+  const selectIcon = () => {
+    switch (
+      value.weather[0].id === 800
+        ? 0
+        : parseInt((value.weather[0].id / 100).toFixed(0))
+    ) {
+      case 0:
+        return <img src={sunny} height="50px" />;
+      case 2:
+        return <img src={thunderstorm} height="50px" />;
+      case 3:
+        return <img src={drizzle} height="50px" />;
+      case 5:
+        return <img src={raining} height="50px" />;
+      case 6:
+        return <img src={snow} height="50px" />;
+      case 7:
+        return <img src={mist} height="50px" />;
+      case 8:
+        return <img src={clouds} height="50px" />;
+    }
+  };
   return (
     <>
-    {value.dt/60/60%6 ==0 ?
-      <td>
-        <tr>{value.dt_txt}</tr>
-        <tr>{selectIcon()}</tr>
-        {/* <p>{value.weather[0].id}</p> */}
-        {/* <p>{(parseInt(value.weather[0].id) / 100).toFixed(0)}</p> */}
-        <tr>{value.main.temp}도</tr>
-        </td>:<></>
-        }
+      {(value.dt / 60 / 60) % 6 == 0 ? (
+        <td>
+          <tr>{value.dt_txt}</tr>
+          <tr>{selectIcon()}</tr>
+          {/* <p>{value.weather[0].id}</p> */}
+          {/* <p>{(parseInt(value.weather[0].id) / 100).toFixed(0)}</p> */}
+          <tr>{value.main.temp}도</tr>
+        </td>
+      ) : (
+        <></>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default WeatherCard
+export default WeatherCard;
--- campingbeaver/src/pages/Recommendation/WeatherView.js
+++ campingbeaver/src/pages/Recommendation/WeatherView.js
@@ -1,88 +1,89 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import style from './Reservation.module.scss'
-import { TiWeatherSunny, TiWeatherCloudy, TiWeatherShower, TiWeatherDownpour, TiWeatherSnow, TiWeatherStormy } from "react-icons/ti";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import style from "./Reservation.module.scss";
+import {
+  TiWeatherSunny,
+  TiWeatherCloudy,
+  TiWeatherShower,
+  TiWeatherDownpour,
+  TiWeatherSnow,
+  TiWeatherStormy,
+} from "react-icons/ti";
 import { BsCloudFog } from "react-icons/bs";
-import WeatherCard from './WeatherCard';
+import WeatherCard from "./WeatherCard";
 
-const WeatherView = ({latitude, longitude}) => {
+const WeatherView = ({ latitude, longitude }) => {
+  let url = "";
 
-    let url = ''
+  const [des, setDes] = useState("");
+  const [temp, setTemp] = useState(0);
+  const [id, setId] = useState();
+  const [iconId, setIconId] = useState();
+  const [weatherList, setWeatherList] = useState([]);
 
-    const [des, setDes] = useState('')
-    const [temp, setTemp] = useState(0)
-    const [id, setId] = useState()
-    const [iconId, setIconId] = useState()
-    const [weatherList, setWeatherList] = useState([])
+  // const getCurrentLocation = () => {
+  //     navigator.geolocation.getCurrentPosition(()=>{
+  //         getWeatherData(latitude,longitude)
+  //     })
+  // }
+  const getWeatherData = () => {
+    url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${"c2cba6fdf07202e294f82e9303beffff"}&units=metric`;
+    axios
+      .get(url)
+      .then((res) => {
+        // console.log(res.data.list)
+        setWeatherList(res.data.list);
+        console.log("weatherlist:", weatherList);
+        // setCity(res.data.name)
+        // setDes(res.data.weather[0].description)
+        // setTemp(res.data.main.temp)
+        // setId(res.data.weather[0].id)
+        // console.log(id)
+        // setIconId(id === 800 ? 0 : (parseInt(id) / 100).toFixed(0));
+        // console.log(iconId)
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  };
 
-    // const getCurrentLocation = () => {
-    //     navigator.geolocation.getCurrentPosition(()=>{
-    //         getWeatherData(latitude,longitude)
-    //     })
-    // }
-    const getWeatherData = () => {
-        url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${'c2cba6fdf07202e294f82e9303beffff'}&units=metric`
-        axios.get(url)
-            .then((res)=>{
-                // console.log(res.data.list)
-                setWeatherList(res.data.list)
-                    console.log('weatherlist:',weatherList)
-                // setCity(res.data.name)
-                // setDes(res.data.weather[0].description)
-                // setTemp(res.data.main.temp)
-                // setId(res.data.weather[0].id)
-                // console.log(id)
-                // setIconId(id === 800 ? 0 : (parseInt(id) / 100).toFixed(0));
-                // console.log(iconId) 
-            })
-            .catch(()=>{console.log('error')})
+  const selectIcon = () => {
+    console.log("switch문", iconId);
+
+    switch (iconId) {
+      case 0:
+        return <TiWeatherSunny size="5rem" color="red" />;
+      case 2:
+        return <TiWeatherStormy size="5rem" color="black" />;
+      case 3:
+        return <TiWeatherShower size="5rem" color="blue" />;
+      case 5:
+        return <TiWeatherDownpour size="5rem" color="navy" />;
+      case 6:
+        return <TiWeatherSnow size="5rem" color="white" />;
+      case 7:
+        return <BsCloudFog size="5rem" color="white" />;
+      case 8:
+        return <TiWeatherCloudy size="5rem" color="white" />;
     }
+  };
 
-    
-    const selectIcon = () => {
-        console.log("switch문",iconId)
-    
+  useEffect(() => {
+    getWeatherData();
+  }, [latitude, longitude]);
 
-        
-        switch (iconId) {
-            case 0:
-              return <TiWeatherSunny size="5rem" color="red" />;
-            case 2:
-              return <TiWeatherStormy size="5rem" color="black" />;
-            case 3:
-              return <TiWeatherShower size="5rem" color="blue" />;
-            case 5:
-              return <TiWeatherDownpour size="5rem" color="navy" />;
-            case 6:
-              return <TiWeatherSnow size="5rem" color="white" />;
-            case 7:
-              return <BsCloudFog size="5rem" color="white" />;
-            case 8:
-              return <TiWeatherCloudy size="5rem" color="white" />;
-          }
-      
-      };
-   
-    
-    useEffect(()=>{
-        getWeatherData()
-      },[latitude,longitude])
-      
-      console.log(weatherList)
+  console.log(weatherList);
   return (
     <div className={style.weather}>
-        
       <table>
         {/* {selectIcon()} */}
-       {/* response.data.filter(value=>value.pkg_hash.includes(selectParams())) */}
-        {weatherList.map(
-            value => (<WeatherCard value={value}/>)
-        )}
+        {/* response.data.filter(value=>value.pkg_hash.includes(selectParams())) */}
+        {weatherList.map((value) => (
+          <WeatherCard value={value} />
+        ))}
       </table>
-        
-        
     </div>
-  )
-}
+  );
+};
 
-export default WeatherView
+export default WeatherView;
